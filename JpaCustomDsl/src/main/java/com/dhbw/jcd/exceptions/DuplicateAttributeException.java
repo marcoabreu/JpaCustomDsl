315db44677b7a24bbc24ed242c49963c24a3d308
Marco <Marco.G.Abreu@gmail.com>
2016-12-04 23:51:05 +0100

    Added JOIN-Query

--- JpaCustomDsl/src/main/java/com/dhbw/jcd/exceptions/DuplicateAttributeException.java
+++
@@ -1,9 +0,0 @@
-package com.dhbw.jcd.exceptions;
-
-public class DuplicateAttributeException extends JcdException {
-
-	public DuplicateAttributeException(Class clazz, String attributeName) {
-		// TODO Auto-generated constructor stub
-	}
-
-}
